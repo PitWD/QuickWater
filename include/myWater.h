4b1641b18b113b448a46f6c33ac84b8bf09619d7
PitWD <pit@lets.de>
2023-04-16 05:36:39 +0200

    HUM & CO2 vars and constance removed

--- include/myWater.h
+++ include/myWater.h
@@ -19,6 +19,7 @@ static byte ezoAct = 0;
 // How many ezo's we have
 static byte ezoCnt = 0;
 
+/*
 #define ezoHumT 0
 #define ezoRTD 1
 #define ezoPH 2
@@ -27,81 +28,61 @@ static byte ezoCnt = 0;
 #define ezoHUM 5
 #define ezoCO2 6
 #define ezoDiO2 7
-#define ezoFLOW 8
-#define ezoRGB 9
-#define ezoPRES 10
+*/
+#define ezoRTD 0
+#define ezoPH 1
+#define ezoEC 2
+#define ezoORP 3
+#define ezoDiO2 4
 
-const char ezoStrType_0[] PROGMEM = "HumT";
-const char ezoStrType_1[] PROGMEM = "RTD";
-const char ezoStrType_2[] PROGMEM = "pH";
-const char ezoStrType_3[] PROGMEM = "EC";
-const char ezoStrType_4[] PROGMEM = "ORP";
-const char ezoStrType_5[] PROGMEM = "HUM%";
-const char ezoStrType_6[] PROGMEM = "CO2";
-const char ezoStrType_7[] PROGMEM = "D.O.";
-// Remove-III const char ezoStrType_8[] PROGMEM = "Flow";
-// Remove-III const char ezoStrType_9[] PROGMEM = "RGB";
-// Remove-III const char ezoStrType_10[] PROGMEM = "Pres";
+
+const char ezoStrType_0[] PROGMEM = "RTD";
+const char ezoStrType_1[] PROGMEM = "pH";
+const char ezoStrType_2[] PROGMEM = "EC";
+const char ezoStrType_3[] PROGMEM = "ORP";
+const char ezoStrType_4[] PROGMEM = "D.O.";
 PGM_P const ezoStrType[] PROGMEM = {
     ezoStrType_0,
     ezoStrType_1,
     ezoStrType_2,
     ezoStrType_3,
-    ezoStrType_4,
-    ezoStrType_5,
-    ezoStrType_6,
-    ezoStrType_7,
-    // Remove-III ezoStrType_8,
-    // Remove-III ezoStrType_9,
-    // Remove-III ezoStrType_10
+    ezoStrType_4
 };
 
-const char ezoStrLongType_0[] PROGMEM = "Air-Temp.";
-const char ezoStrLongType_1[] PROGMEM = "H2O-Temp.";
+const char ezoStrLongType_0[] PROGMEM = "Temp.";
 // pH
 // EC
-const char ezoStrLongType_4[] PROGMEM = "Redox";
-const char ezoStrLongType_5[] PROGMEM = "Humidity";
-// CO2
-const char ezoStrLongType_7[] PROGMEM = "H2O-O2";
+const char ezoStrLongType_3[] PROGMEM = "Redox";
+const char ezoStrLongType_4[] PROGMEM = "O2";
 PGM_P const ezoStrLongType[] PROGMEM = {
     ezoStrLongType_0,
-    ezoStrLongType_1,
+    ezoStrType_1,
     ezoStrType_2,
-    ezoStrType_3,
-    ezoStrLongType_4,
-    ezoStrLongType_5,
-    ezoStrType_6,
-    ezoStrLongType_7,   
+    ezoStrLongType_3,
+    ezoStrLongType_4
 };
 
-//const char ezoStrUnit_0[] PROGMEM = "°C";
-const char ezoStrUnit_1[] PROGMEM = "°C";
-//const char ezoStrUnit_2[] PROGMEM = "pH";
-const char ezoStrUnit_3[] PROGMEM = "µS";
-const char ezoStrUnit_4[] PROGMEM = "mV";
-const char ezoStrUnit_5[] PROGMEM = "r%";
-const char ezoStrUnit_6[] PROGMEM = "ppm";
-const char ezoStrUnit_7[] PROGMEM = "%";
+const char ezoStrUnit_0[] PROGMEM = "°C";
+// "pH";
+const char ezoStrUnit_2[] PROGMEM = "µS";
+const char ezoStrUnit_3[] PROGMEM = "mV";
+const char ezoStrUnit_4[] PROGMEM = "%";
 PGM_P const ezoStrUnit[] PROGMEM = {
-    ezoStrUnit_1,
-    ezoStrUnit_1,
+    ezoStrUnit_0,
+    ezoStrType_1,
     ezoStrType_2,
     ezoStrUnit_3,
-    ezoStrUnit_4,
-    ezoStrUnit_5,
-    ezoStrUnit_6,
-    ezoStrUnit_7
+    ezoStrUnit_4
 };
 
 // Waittime for readings...
-const int ezoWait[] PROGMEM = {0, 600, 900, 600, 900, 300, 900, 600}; // Remove-III , 300, 300, 900};
+const int ezoWait[] PROGMEM = {600, 900, 600, 900, 600};
 
 // Count of vals of probe
-const byte ezoValCnt[] PROGMEM = {0, 1, 1, 1, 1, 3, 2, 2}; // Remove-III  2, 5, 1};
+const byte ezoValCnt[] PROGMEM = {1, 1, 1, 1, 2};
 
 // if type has a calibration
-const byte ezoHasCal[] PROGMEM = {0, 1, 1, 1, 1, 0, 0, 0}; // Remove-III  0, 0, 0};
+const byte ezoHasCal[] PROGMEM = {1, 1, 1, 1, 0};
 
 typedef struct ezoProbeSTRUCT{
     byte type;
@@ -116,80 +97,62 @@ typedef struct ezoProbeSTRUCT{
 ezoProbeSTRUCT ezoProbe[EZO_MAX_PROBES];
 
 // Delay & ActionTimes
-uint16_t delayTimes[] = {1800, 2700, 2400, 2400, 0, 1800, 3600, 0};
-uint16_t actionTooLow[] = {3600, 900, 6, 10, 0, 900, 450, 0};
-uint16_t actionLow[] = {1800, 450, 3, 5, 0, 450, 225, 0};
-uint16_t actionHigh[] = {1800, 900, 3, 30, 0, 900, 0, 0};
-uint16_t actionTooHigh[] = {3600, 1800, 6, 60, 0, 2700, 0, 0};
+uint16_t delayTimes[] = {2700, 2400, 2400, 0, 0};
+uint16_t actionTooLow[] = {900, 6, 10, 0, 0};
+uint16_t actionLow[] = {450, 3, 5, 0, 0};
+uint16_t actionHigh[] = {900, 3, 30, 0, 0};
+uint16_t actionTooHigh[] = {1800, 6, 60, 0, 0};
 
 // Counter for Low/High
-uint32_t tooLowSince[8];
-uint32_t lowSince[8];
-uint32_t okSince[8];
-uint32_t highSince[8];
-uint32_t tooHighSince[8];
+uint32_t tooLowSince[5];
+uint32_t lowSince[5];
+uint32_t okSince[5];
+uint32_t highSince[5];
+uint32_t tooHighSince[5];
 // Time of last action 
-uint32_t lastAction[8];
+uint32_t lastAction[5];
 
-long failSave[] = {25000L, 21000L, 6000L, 1250000L, 225000L, 50000L, 400000L, 99999};
-#define failSave_TMP failSave[0]
-#define failSave_RTD failSave[1]
-#define failSave_pH failSave[2]
-#define failSave_EC failSave[3]
-#define failSave_ORP failSave[4]
-#define failSave_HUM failSave[5]
-#define failSave_CO2 failSave[6]
-#define failSave_O2 failSave[7]
+long failSave[] = {21000L, 6000L, 1250000L, 225000L, 99999};
+#define failSave_RTD failSave[0]
+#define failSave_pH failSave[1]
+#define failSave_EC failSave[2]
+#define failSave_ORP failSave[3]
+#define failSave_O2 failSave[4]
 
-long avgVal[] = {25000L, 21000L, 6000L, 1250000L, 225000L, 50000L, 400000L, 99999};
-#define avg_TMP avgVal[0]
-#define avg_RTD avgVal[1]
-#define avg_pH avgVal[2]
-#define avg_EC avgVal[3]
-#define avg_ORP avgVal[4]
-#define avg_HUM avgVal[5]
-#define avg_CO2 avgVal[6]
-#define avg_O2 avgVal[7]
+long avgVal[] = {21000L, 6000L, 1250000L, 225000L, 99999};
+#define avg_RTD avgVal[0]
+#define avg_pH avgVal[1]
+#define avg_EC avgVal[2]
+#define avg_ORP avgVal[3]
+#define avg_O2 avgVal[4]
 
-long tooLow[] = {16000L, 15000L, 5500L, 1000000L, -750000L, 40000L, 350000L, 50000L};
-#define tooLow_TMP tooLow[0]    // 2nd Val ezoHUM
-#define tooLow_RTD tooLow[1]
-#define tooLow_pH tooLow[2]
-#define tooLow_EC tooLow[3]
-#define tooLow_ORP tooLow[4]
-#define tooLow_HUM tooLow[5]
-#define tooLow_CO2 tooLow[6]
-#define tooLow_O2 tooLow[7]
+long tooLow[] = {15000L, 5500L, 1000000L, -750000L, 50000L};
+#define tooLow_RTD tooLow[0]
+#define tooLow_pH tooLow[1]
+#define tooLow_EC tooLow[2]
+#define tooLow_ORP tooLow[3]
+#define tooLow_O2 tooLow[4]
 
-long low[] = {19000L, 17000L, 5800L, 1250000L, -500000L, 50000L, 400000L, 66666L};
-#define low_TMP low[0]      // 2nd Val ezoHUM
-#define low_RTD low[1]
-#define low_pH low[2]
-#define low_EC low[3]
-#define low_ORP low[4]
-#define low_HUM low[5]
-#define low_CO2 low[6]
-#define low_O2 low[7]
+long low[] = {17000L, 5800L, 1250000L, -500000L, 66666L};
+#define low_RTD low[0]
+#define low_pH low[1]
+#define low_EC low[2]
+#define low_ORP low[3]
+#define low_O2 low[4]
 
-long high[] = {25000L, 20000L, 6800L, 1750000L, 500000L, 66666L, 1250000L, 100001L};
-#define high_TMP high[0]    // 2nd Val ezoHUM
-#define high_RTD high[1]
-#define high_pH high[2]
-#define high_EC high[3]
-#define high_ORP high[4]
-#define high_HUM high[5]
-#define high_CO2 high[6]
-#define high_O2 high[7]
+long high[] = {20000L, 6800L, 1750000L, 500000L, 100001L};
+#define high_RTD high[0]
+#define high_pH high[1]
+#define high_EC high[2]
+#define high_ORP high[3]
+#define high_O2 high[4]
 
-long tooHigh[] = {27000L, 22000L, 7000L, 2000000L, 750000L, 75000L, 1500000L, 100001L};
-#define tooHigh_TMP tooHigh[0]    // 2nd Val ezoHUM
-#define tooHigh_RTD tooHigh[1]
-#define tooHigh_pH tooHigh[2]
-#define tooHigh_EC tooHigh[3]
-#define tooHigh_ORP tooHigh[4]
-#define tooHigh_HUM tooHigh[5]
-#define tooHigh_CO2 tooHigh[6]
-#define tooHigh_O2 tooHigh[7]
+long tooHigh[] = {22000L, 7000L, 2000000L, 750000L, 100001L};
+#define tooHigh_RTD tooHigh[0]
+#define tooHigh_pH tooHigh[1]
+#define tooHigh_EC tooHigh[2]
+#define tooHigh_ORP tooHigh[3]
+#define tooHigh_O2 tooHigh[4]
 
 
 #define CAL_RTD_RES -1L         // Value for Reset
@@ -356,41 +319,6 @@ void EzoReset(byte ezo, byte all){
                 strcpy_P(strSetup[2],(PGM_P)F("O,%,1"));
                 cntSetup = 3;
                 break;
-            case ezoHUM:
-                strcpy_P(strSetup[1],(PGM_P)F("O,HUM,1")); // Humidity
-                strcpy_P(strSetup[2],(PGM_P)F("O,T,1"));   // Temperature 
-                strcpy_P(strSetup[3],(PGM_P)F("O,Dew,1")); // Dewing point
-                strcpy_P(strSetup[4],(PGM_P)F("Alarm,en,0"));
-                cntSetup = 5;
-                break;
-            /* // Remove-III 
-            case ezoFLOW:
-                strcpy_P(strSetup[1],(PGM_P)F("Frp,m"));    // FlowRate/minute (s = second / h = hour)
-                strcpy_P(strSetup[2],(PGM_P)F("CF,0.001")); // Liter (1 would be ml)
-                strcpy_P(strSetup[3],(PGM_P)F("O,TV,1"));   // Output Total Volume
-                strcpy_P(strSetup[4],(PGM_P)F("O,FR,1"));   // Output Flow Rate
-                cntSetup = 5;
-                break;
-            case ezoPRES:
-                strcpy_P(strSetup[1],(PGM_P)F("Dec,3"));
-                strcpy_P(strSetup[2],(PGM_P)F("U,bar"));
-                strcpy_P(strSetup[3],(PGM_P)F("Alarm,en,0"));
-                cntSetup = 4;
-                break;
-            case ezoRGB:
-                strcpy_P(strSetup[0],(PGM_P)F("iL,1"));
-                strcpy_P(strSetup[1],(PGM_P)F("O,RGB,1"));
-                strcpy_P(strSetup[2],(PGM_P)F("O,LUX,1"));
-                strcpy_P(strSetup[3],(PGM_P)F("O,CIE,1"));
-                strcpy_P(strSetup[4],(PGM_P)F("L,33,T"));
-                cntSetup = 5;
-                break;
-            */ // Remove-III 
-            case ezoCO2:
-                strcpy_P(strSetup[1],(PGM_P)F("O,t,1"));   // Output Internal Temp
-                cntSetup = 2;
-                break;
-            
             default:
                 break;
             }
@@ -595,13 +523,6 @@ void EzoScan(){
                         case 'R':
                             // RGB or RTD
                             switch (iicStr[4]){
-                            /* // Remove-III 
-                            case 'G':
-                                // RGB
-                                recEzo = ezoRGB;
-                                hasCal = 0;
-                                break;
-                            */ // Remove-III 
                             case 'T':
                                 // RTD
                                 recEzo = ezoRTD;
@@ -611,27 +532,10 @@ void EzoScan(){
                                 break;
                             }
                             break;
-                        /* // Remove-III 
-                        case 'F':
-                            // FLO(W)
-                            recEzo = ezoFLOW;
-                            hasCal = 0;
-                            break;
-                        */ // Remove-III 
                         case 'O':
                             // ORP
                             recEzo = ezoORP;
                             break;
-                        case 'C':
-                            // CO2
-                            recEzo = ezoCO2;
-                            hasCal = 0;
-                            break;
-                        case 'H':
-                            // HUM
-                            recEzo = ezoHUM;
-                            hasCal = 0;
-                            break;    
                         case 'E':
                             // EC
                             recEzo = ezoEC;
@@ -647,14 +551,8 @@ void EzoScan(){
                             recEzo = ezoDiO2;
                             verPos = 8;
                             break;
-                        /* // Remove-III 
-                        case 'P':
-                            // Embedded Pressure
-                            recEzo = ezoPRES;
-                            break;
-                        */ // Remove-III 
                         default:
-                            // LATE ERROR
+                            // LATE ERROR or not supported...
                             break;
                         }
                         Serial.println(recEzo);
